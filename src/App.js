ec200b6d1a6d57ce7847c62af7c2198e1d29f4d2
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-05-16 07:59:28 -0700

    About added with extra and unique Nav categories-console checked

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
 import './App.css';
-
+{/*import logo from './logo.svg';*/}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*NAV IN PASCAL BECOMES ITS OWN ELEMENT ABOVE MAIN*/}
+      <Nav></Nav>
+        {/* REMOVE THE REACT TEMPLATE CONTENT INSIDE THE <DIV> CONTAINER ELEMENT AND ADD A <MAIN> ELEMENT TO CONTAIN YOUR NEW COMPONENTS */}
+        <main>
+          <About></About>
+        </main>
     </div>
   );
 }
