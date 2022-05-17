a59f4764eb65a8d49d989e9c3639ac901b2bc81b
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-05-17 03:41:22 -0700

    hooks initiated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
@@ -6,10 +6,34 @@ import './App.css';
 // import './index-clc.css';
 {/*import logo from './logo.svg';*/ }
 function App() {
+  //ADD CATEGORIES - GO THE EXTRA MILE TO USE IT LATER
+  const [categories] = useState([
+    {
+      name: "business-finance",
+      description:
+        "Digital asset tracker app; physical offices,bank, and a medical project",
+    },
+    { name: "developments-bldgs", description: "Digital team projects; physical stand-alone building projects" },
+    { name: "health-wellness", description: "Digital conversation and excercise practice app; physical medical and healthclub facilities" },
+    { name: "individual-residential", description: "Digital expression tools; physical private residences" },
+    { name: "refactor-renovate", description: "Digital accessibility; physical typical energy efficiency and accessibility improvements" },
+    { name: "retail-restaurant", description: "Digital foodie and retail apps; physical stores and restaurants" },
+    { name: "testing-storage", description: "Digital R&D apps; physical testing facilities" },
+    {
+      name: "travel-leisure",
+      description: "Digital planet and math random apps; physical adventure places"
+    }
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div className="App">
       {/*NAV IN PASCAL BECOMES ITS OWN ELEMENT ABOVE MAIN*/}
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       {/* REMOVE THE REACT TEMPLATE CONTENT INSIDE THE <DIV> CONTAINER ELEMENT AND ADD A <MAIN> ELEMENT TO CONTAIN YOUR NEW COMPONENTS */}
       <main>
         <Gallery></Gallery>
